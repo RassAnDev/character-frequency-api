b0c66ec9eeb8a01a73cc30039bbb6f89c90b2520
Andrew <rassandev@gmail.com>
2023-11-22 15:21:26 +0300

    add application main class

---
+++ src/main/java/com/frequency/characterfrequencyapi/CharacterFrequencyApiApplication.java
@@ -0,0 +1,13 @@
+package com.frequency.characterfrequencyapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CharacterFrequencyApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CharacterFrequencyApiApplication.class, args);
+    }
+
+}
